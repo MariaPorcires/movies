ef39b4ceac187c9473b100993d38a54ebc4975ae
hellnomary <maria.porcires@outlook.com>
2023-08-11 12:18:44 +0200

    API

---
+++ movies/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { useEffect } from 'react'
+
+const API_URL = "http://www.omdbapi.com?apikey=632729cd"
+
+function App() {
+  
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
