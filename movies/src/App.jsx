868b30b2159325eb01beaf1bf346b124d67cd16d
hellnomary <maria.porcires@outlook.com>
2023-08-11 12:50:48 +0200

    API works

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -2,10 +2,18 @@
 import './App.css'
 import { useEffect } from 'react'
 
-const API_URL = "http://www.omdbapi.com?apikey=632729cd"
+const API_URL = 'http://www.omdbapi.com?apikey=632729cd';
 
 function App() {
-  
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`); //&s separerar
+    const data = await response.json()
+    console.log(data.Search)
+  }
+
+  useEffect(() => {
+    searchMovies('Lord of the rings')
+  },[])
 
   return (
     <>
