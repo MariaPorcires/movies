d0ef1b8ae8e3d9a6a7319bc69a3c51f0fe4e7133
hellnomary <maria.porcires@outlook.com>
2023-08-13 11:50:31 +0200

    search function

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -1,23 +1,18 @@
 
 import './App.css'
 import { useEffect } from 'react'
+import SearchMovies from './components/SearchMovies/SearchMovies';
 
-const API_URL = 'http://www.omdbapi.com?apikey=632729cd';
+
 
 function App() {
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`); //&s separerar
-    const data = await response.json()
-    console.log(data.Search)
-  }
+ 
+  
 
-  useEffect(() => {
-    searchMovies('Lord of the rings')
-  },[])
 
   return (
     <>
-
+      <SearchMovies />
     </>
   )
 }
--- movies/src/components/SearchMovies/SearchMovies.jsx
+++ movies/src/components/SearchMovies/SearchMovies.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react";
 
 function SearchMovies() {
+    const [title, setTitle] = useState('')
+
+    async function handleClick() {
+    
+        const response = await fetch(`http://www.omdbapi.com?apikey=632729cd&s=${title}`); //&s separerar
+        const data = await response.json()
+        console.log(data.Search)
+    }
+
+
     return(
-        <></>
+        <>
+        <input type="text" onChange={(event) => setTitle(event.target.value)}></input> 
+        <button onClick={ handleClick }>Search Movie</button>
+        </>
     )
-}
+}//hämtar värdet från input, funktion direkt i input
 
 export default SearchMovies
